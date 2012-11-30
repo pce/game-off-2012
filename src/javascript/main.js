91b5228574f95c18cdaf545ebae31412ae73b4dd
pce <monzens@gmail.com>
2012-11-30 19:25:48 +0100

    Audio Support ogg and WAV

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -23,6 +23,30 @@ function rnd(min, max) {
     return Math.floor((Math.random() * max) + min);
 }
 
+function audio() {
+	// from Modernizr.audio
+	var elem = document.createElement('audio'),
+        bool = false;
+    try {
+        if ( bool = !!elem.canPlayType ) {
+            bool      = new Boolean(bool);
+            bool.ogg  = elem.canPlayType('audio/ogg; codecs="vorbis"').replace(/^no$/,'');
+            bool.mp3  = elem.canPlayType('audio/mpeg;')               .replace(/^no$/,'');
+            bool.wav  = elem.canPlayType('audio/wav; codecs="1"')     .replace(/^no$/,'');
+            bool.m4a  = ( elem.canPlayType('audio/x-m4a;')            ||
+                          elem.canPlayType('audio/aac;'))             .replace(/^no$/,'');
+        }
+    } catch(e) { }
+    return bool;
+}
+
+function getExtAudio() {
+	if (audio().ogg) {
+		return ".ogg";	
+	}
+	return ".wav";
+}
+
 function main() {
 
     var gameState = 0;
@@ -193,7 +217,9 @@ function main() {
 
 
 var sounds = ["start", "sheep", "hit", "last-hit", "success"];
-var extAudio = ".wav"; // || ".ogg"
+
+
+var extAudio = getExtAudio();
 for (var s in sounds) {
  	sounds[s] = 'data/' + sounds[s] + extAudio;
 }
