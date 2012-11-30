6631938839abfa4a2b30eb1cc6999eb913c6ff03
pce <monzens@gmail.com>
2012-11-30 21:10:09 +0100

    new sheep pickup sound

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -23,25 +23,9 @@ function rnd(min, max) {
     return Math.floor((Math.random() * max) + min);
 }
 
-function audio() {
-	// from Modernizr.audio
-	var elem = document.createElement('audio'),
-        bool = false;
-    try {
-        if ( bool = !!elem.canPlayType ) {
-            bool      = new Boolean(bool);
-            bool.ogg  = elem.canPlayType('audio/ogg; codecs="vorbis"').replace(/^no$/,'');
-            bool.mp3  = elem.canPlayType('audio/mpeg;')               .replace(/^no$/,'');
-            bool.wav  = elem.canPlayType('audio/wav; codecs="1"')     .replace(/^no$/,'');
-            bool.m4a  = ( elem.canPlayType('audio/x-m4a;')            ||
-                          elem.canPlayType('audio/aac;'))             .replace(/^no$/,'');
-        }
-    } catch(e) { }
-    return bool;
-}
 
 function getExtAudio() {
-	if (audio().ogg) {
+	if ((new Audio()).canPlayType("audio/ogg; codecs=vorbis") == "probably") {
 		return ".ogg";	
 	}
 	return ".wav";
